1e9068ddf6979cc951f34cb651c29cc55f4ac8c4
Takasur Azeem <takasurazeem@gmail.com>
2022-07-27 00:44:38 +0500

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Takasur Azeem on 27/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
