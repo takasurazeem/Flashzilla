ca8da8890dbb717c64e4180660af6451ce604d4b
Takasur Azeem <takasurazeem@gmail.com>
2022-07-29 18:39:02 +0500

    Challenge 1: When adding a new card empty fields after adding the card.
    Signed-off-by: Takasur Azeem <takasurazeem@gmail.com>

--- Flashzilla/EditCards.swift
+++ Flashzilla/EditCards.swift
@@ -69,6 +69,9 @@ struct EditCards: View {
         let card = Card(prompt: trimmedPrompt, answer: trimmedAnswer)
         cards.insert(card, at: 0)
         saveData()
+        // Clear Text Fields
+        newPrompt = ""
+        newAnswer = ""
     }
     
     func removeCards(at offsets: IndexSet) {
