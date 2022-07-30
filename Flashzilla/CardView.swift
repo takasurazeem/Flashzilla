092c863e057b87b99019e853d7443e8a0e6624ff
Takasur Azeem <takasurazeem@gmail.com>
2022-07-30 17:57:49 +0500

    Challenge 2: If you drag a card to the right but not far enough to remove it, then release, you see it turn red as it slides back to the center. Why does this happen and how can you fix it? (Tip: think about the way we set offset back to 0 immediately, even though the card hasn’t animated yet. You might solve this with a ternary within a ternary, but a custom modifier will be cleaner.)
    Signed-off-by: Takasur Azeem <takasurazeem@gmail.com>

--- Flashzilla/CardView.swift
+++ Flashzilla/CardView.swift
@@ -32,7 +32,7 @@ struct CardView: View {
                     ? nil
                     :
                     RoundedRectangle(cornerRadius: 25, style: .continuous)
-                        .fill(offset.width > 0 ? .green : .red)
+                        .fill(offset.width == 0 ? .white : offset.width > 0 ? .green : .red)
                 )
                 .shadow(radius: 10)
                 .onTapGesture {
